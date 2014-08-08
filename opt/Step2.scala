13f508312756929a584959abbf9ba3bbc9a5835a
Nick <nick@stabtechs.com>
2014-08-08 14:33:07 -0700

    hack for gametime..

---
+++ opt/Step2.scala
@@ -0,0 +1,5 @@
+import upickle._
+
+package object fuckit {
+  //implicit def YOU_FUCKED_IT[T]: Reader[T] = { assert(false,"FUCKIT EXECUTED!") ; ??? }; implicit def STAB_WRITER[T]: Writer[T] = { assert(false,"FUCKIT EXECUTED!") ; ??? }
+}
